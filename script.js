001e3246c52f3c2ea3f9ba1c9d2c53cf65d36c8c
quentu <qustoy@proton.me>
2023-12-13 23:53:23 -0800

    dark mode experiments

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById('secretImage').addEventListener('click', function() {
     var audio = document.getElementById('secretAudio');
     audio.play();
 });
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+  
