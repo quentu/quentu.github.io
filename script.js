202cd2142faeebc1b682b7106af833dafa5ad438
quentu <qustoy@proton.me>
2024-02-19 20:55:29 -0800

    finished about,  dark mode toggle, projects tba

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-document.getElementById('secretImage').addEventListener('click', function() {
-    var audio = document.getElementById('secretAudio');
-    audio.play();
-});
-
-function myFunction() {
+function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
