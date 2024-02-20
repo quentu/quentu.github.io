db1b4120ae292911f72367e5211ef711213cbd52
quentu <qustoy@proton.me>
2024-02-19 22:05:23 -0800

    modified script.js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function darkMode() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    var darkModeCheckbox = document.getElementById("darkMode");
+    var isDarkMode = darkModeCheckbox.checked;
+
+    if (isDarkMode) {
+        element.classList.add("dark-mode");
+    } else {
+        element.classList.remove("dark-mode");
+    }
+    localStorage.setItem("darkMode", isDarkMode);
 }
+
+function checkDarkModePreference() {
+    var darkModeCheckbox = document.getElementById("darkMode");
+    var isDarkMode = localStorage.getItem("darkMode");
+
+    darkModeCheckbox.checked = isDarkMode === "true";
+
+    if (isDarkMode === "true") {
+        document.body.classList.add("dark-mode");
+    }
+}
+
+window.onload = checkDarkModePreference;
+
+document.getElementById("darkMode").addEventListener("change", darkMode);
   
